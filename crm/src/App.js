c71b40c1db1a0f14602ece587199d82f4ecc2d4e
subhranta_703 <subhrantanayak6535@gmail.com>
2024-11-16 21:57:11 +0530

    Initial commit

---
+++ crm/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold mt-56 text-center ">
+      !!! Har Har Mahadev !!!
+    </h1>
+  );
+}
+
+export default App;
